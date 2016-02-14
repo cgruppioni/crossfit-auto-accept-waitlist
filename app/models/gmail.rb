6e73968413f141f8a8335c3a285f4338a25789dd
Casey Gruppioni <casey@appcues.com>
2016-02-13 21:11:31 -0500

    makes request succesfully

--- app/models/gmail.rb
+++ app/models/gmail.rb
@@ -6,31 +6,38 @@ require 'gmail'
 require 'pry-rails'
 require "dotenv" ; Dotenv.load
 require 'pry'
-require 'nokogiri'
+require 'net/http'
 
 class Wodify
   def initialize
     Gmail.new(ENV["GMAIL_USERNAME"], ENV['GMAIL_PASSWORD']) do |gmail|
-
       emails = gmail.inbox.emails
-      #message includes from, to and subject
-      # message = emails.first.message
-      #body includes message
-      # body = emails.first.body
 
-      # wodify_emails(emails).each do |email|
+      wodify_emails = get_emails(emails)
+      link = get_link(wodify_emails)
 
-      wodify_emails(emails).first.body.to_s.scan( /<([^>]*)>/).each do |section|
-        if section.first.include? 'a id=3D"wt4"'
-           link = section.first.gsub("=\n", "")
-           puts link.sub!(/.*?(?="http)/im, "")
-        end
-      end
-
+      Net::HTTP.get(URI(link))
     end
   end
 
-  def wodify_emails(emails)
+
+  #right now this calls .first and is only getting the first email
+  def get_link(emails)
+    link = ""
+
+    emails.first.body.to_s.scan( /<([^>]*)>/).each do |section|
+      if section.first.include? 'a id=3D"wt4"'
+         #get rid of new lines
+         #get rid of the substring before the link starts
+         #get rid of extra '/"''s
+         link = section.first.gsub("=\n", "").sub!(/.*?(?="http)/im, "").tr('\"', "")
+      end
+    end
+
+    link
+  end
+
+  def get_emails(emails)
     wodify_emails = []
 
     emails.each do |email|
